9f6cde7750da1ce99ac7fbb93701c3400e40363b
Maria Schneider <maria.schneider.aug@gmail.com>
2022-09-30 18:52:58 +0200

    Task 2

--- Seminars/Seminar1/Program.cs
+++ Seminars/Seminar1/Program.cs
@@ -53,17 +53,17 @@ while(current <= number)
 // Задача 3. Напишите программу, которая принимает на вход трёхзначное число 
 // и на выходе показывает последнюю цифру этого числа.
 
-Сonsole.Write("Input three-digit number: ");
+Console.Write("Input three-digit number: ");
 int n = Convert.ToInt32(Console.ReadLine());
 
-if(n >= 100 && < 1000)
+if(n >= 100 && n < 1000)
 {
     int lastDigit = n % 10;
     Console.WriteLine($"Last digit of {n} is {lastDigit}");
 }
 else
 {
-    Console.WriteLine("Uncorrwct input!");
+    Console.WriteLine("Uncorrect input!");
 }
 
 
