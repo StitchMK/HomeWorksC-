b244ba0f805125b47b38348dd34593d071856ad8
Maria Schneider <maria.schneider.aug@gmail.com>
2022-09-30 10:54:27 +0200

    First Seminar complete

---
+++ Seminars/Seminar1/Program.cs
@@ -0,0 +1,69 @@
+﻿// Задача 1. Напишите программу, которая на вход принимает два числа и проверяет, 
+// является ли первое число квадратом второго.
+
+/* 
+
+Console.Write("Input a first number: ");
+int n1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input a second number: ");
+int n2 = Convert.ToInt32(Console.ReadLine());
+
+int quad2 = n2 * n2;
+
+if (n1 == quad2)
+{
+    Console.WriteLine("Yes");
+}
+else
+{
+    Console.WriteLine("No");
+}
+
+*/
+
+// Задача 2. Напишите программу, которая на вход принимает одно число (N), 
+// а на выходе показывает все целые числа в промежутке от -N до N.
+
+/* 
+
+Console.WriteLine("Input a number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int current;
+
+if(number > 0)
+{
+    current = -number;
+}
+
+else
+{
+    current = number;
+    number *= -1;
+}
+
+while(current <= number)
+{
+    Console.Write(current + " ");
+    current++;
+}
+
+*/
+
+// Задача 3. Напишите программу, которая принимает на вход трёхзначное число 
+// и на выходе показывает последнюю цифру этого числа.
+
+Сonsole.Write("Input three-digit number: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+if(n >= 100 && < 1000)
+{
+    int lastDigit = n % 10;
+    Console.WriteLine($"Last digit of {n} is {lastDigit}");
+}
+else
+{
+    Console.WriteLine("Uncorrwct input!");
+}
+
+
